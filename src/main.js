9d723781fe859add45deff8d59a777a50d376ec5
DogeisCut <rickisparks06@gmail.com>
2023-04-05 19:13:50 -0600

    Beautified Everything

--- src/main.js
+++ src/main.js
@@ -59,9 +59,15 @@ class SketchCanvas {
     constructor(width, height) {
         this.width = width;
         this.height = height;
-        this.translation = { x: (canvas.width/2)-this.width/2, y: (canvas.height/2)-this.height/2 };
+        this.translation = {
+            x: (canvas.width / 2) - this.width / 2,
+            y: (canvas.height / 2) - this.height / 2
+        };
         this.isPanning = false;
-        this.lastPanPosition = { x: 0, y: 0 };
+        this.lastPanPosition = {
+            x: 0,
+            y: 0
+        };
 
         // add event listeners for panning
         canvas.addEventListener("mousedown", this.handleMouseDown.bind(this));
@@ -74,7 +80,10 @@ class SketchCanvas {
         if (event.button === 1) {
             // middle mouse button pressed
             this.isPanning = true;
-            this.lastPanPosition = { x: event.clientX, y: event.clientY };
+            this.lastPanPosition = {
+                x: event.clientX,
+                y: event.clientY
+            };
         }
     }
 
@@ -85,7 +94,10 @@ class SketchCanvas {
             const dy = event.clientY - this.lastPanPosition.y;
             this.translation.x += dx;
             this.translation.y += dy;
-            this.lastPanPosition = { x: event.clientX, y: event.clientY };
+            this.lastPanPosition = {
+                x: event.clientX,
+                y: event.clientY
+            };
         }
     }
 
@@ -102,47 +114,47 @@ var currentSketchCanvas = new SketchCanvas(800, 600);
 function draw() {
     requestAnimationFrame(draw);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
+
     //Borders 
     ctx.fillStyle = '#303030';
     ctx.strokeStyle = "#00000000"
     ctx.lineWidth = 0;
     //Left
     ctx.beginPath();
-    ctx.moveTo(0+currentSketchCanvas.translation.x,0);
-    ctx.lineTo(0+currentSketchCanvas.translation.x,canvas.height);
-    ctx.lineTo(0,canvas.height);
-    ctx.lineTo(0,0);
+    ctx.moveTo(0 + currentSketchCanvas.translation.x, 0);
+    ctx.lineTo(0 + currentSketchCanvas.translation.x, canvas.height);
+    ctx.lineTo(0, canvas.height);
+    ctx.lineTo(0, 0);
     ctx.fill();
     //Right
     ctx.beginPath();
-    ctx.moveTo(currentSketchCanvas.width+currentSketchCanvas.translation.x,0);
-    ctx.lineTo(currentSketchCanvas.width+currentSketchCanvas.translation.x,canvas.height);
-    ctx.lineTo(canvas.width,canvas.height);
-    ctx.lineTo(canvas.width,0);
+    ctx.moveTo(currentSketchCanvas.width + currentSketchCanvas.translation.x, 0);
+    ctx.lineTo(currentSketchCanvas.width + currentSketchCanvas.translation.x, canvas.height);
+    ctx.lineTo(canvas.width, canvas.height);
+    ctx.lineTo(canvas.width, 0);
     ctx.fill();
     //Bottom
     ctx.beginPath();
-    ctx.moveTo(canvas.width,canvas.height);
-    ctx.lineTo(0,canvas.height);
-    ctx.lineTo(0,currentSketchCanvas.height+currentSketchCanvas.translation.y);
-    ctx.lineTo(canvas.width,currentSketchCanvas.height+currentSketchCanvas.translation.y);
+    ctx.moveTo(canvas.width, canvas.height);
+    ctx.lineTo(0, canvas.height);
+    ctx.lineTo(0, currentSketchCanvas.height + currentSketchCanvas.translation.y);
+    ctx.lineTo(canvas.width, currentSketchCanvas.height + currentSketchCanvas.translation.y);
     ctx.fill();
     //Top
     ctx.beginPath();
-    ctx.moveTo(0,0);
-    ctx.lineTo(canvas.width,0);
-    ctx.lineTo(canvas.width,0+currentSketchCanvas.translation.y);
-    ctx.lineTo(0,0+currentSketchCanvas.translation.y);
+    ctx.moveTo(0, 0);
+    ctx.lineTo(canvas.width, 0);
+    ctx.lineTo(canvas.width, 0 + currentSketchCanvas.translation.y);
+    ctx.lineTo(0, 0 + currentSketchCanvas.translation.y);
     ctx.fill();
 
     //Canvas Border
     ctx.strokeStyle = "#636363"
     ctx.lineWidth = 1;
     ctx.beginPath();
-    ctx.rect(currentSketchCanvas.translation.x, currentSketchCanvas.translation.y,currentSketchCanvas.width,currentSketchCanvas.height);
+    ctx.rect(currentSketchCanvas.translation.x, currentSketchCanvas.translation.y, currentSketchCanvas.width, currentSketchCanvas.height);
     ctx.stroke()
 
 
-  }
-  draw();
+}
+draw();
